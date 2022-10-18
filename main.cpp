456b2a95207ae8c89388a5ba470b05b523960b17
Mariya Troeglazova <hitacomaru@gmail.com>
2022-10-18 21:11:19 +0300

    Project created

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string card_string;
+    std::cout << "Please, enter a card number:\n";
+    std::cin >> card_string;
+    std::cout << "\n";
+    std::cout << card_string;
+    return 0;
+}

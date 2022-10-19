3a5600db87f6c4f445cfeeb6d527e9191be44fbf
Mariya Troeglazova <hitacomaru@gmail.com>
2022-10-19 21:12:03 +0300

    del symbol

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
 
+void delete_symbols(std::string * str, char del_char){
+
+}
+
 int main() {
     std::string card_string;
     std::cout << "Please, enter a card number:\n";
     std::cin >> card_string;
+
+    delete_symbols(&card_string, ' ');
+
     std::cout << "\n";
     std::cout << card_string;
     return 0;

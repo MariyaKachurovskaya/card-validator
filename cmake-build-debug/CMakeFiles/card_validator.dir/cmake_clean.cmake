198696653b10ac200bf8764fa911c5ed7312cd86
mtroeglazova <mtroeglazova@aladdin.ru>
2022-10-26 08:10:11 +0300

    .gitignore

---
+++ cmake-build-debug/CMakeFiles/card_validator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/card_validator.dir/main.cpp.obj"
+  "CMakeFiles/card_validator.dir/main.cpp.obj.d"
+  "card_validator.exe"
+  "card_validator.exe.manifest"
+  "card_validator.pdb"
+  "libcard_validator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/card_validator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/79641/Documents/git/card-validator
+# Install script for directory: C:/Users/mtroeglazova/card-validator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/79641/Documents/git/card-validator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mtroeglazova/card-validator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
